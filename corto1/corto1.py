e3f1429c7ce8f05e0be74de766c3e763c8e6de57
usac201700796 <3007390010101.ingenieria.usac.edu.gt>
2020-06-09 12:37:18 -0600

    Corto final

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,4 +1,38 @@
 #! /usr/bin/python3.8
 
-print ('primer corto')
+
+
+N = 796
+
+def tipodenumero(valor):
+	if valor % 2 == 0:
+		# print ('numero par')
+		valor = valor // 2
+	elif valor %2  !=0:
+		# print ('numero impar')
+		valor = 3*valor + 1
+
+	return valor
+
+def secuencia (G):
+	h = []
+	h.append(G)
+	while G != 1:
+		G = tipodenumero(G)
+		h.append(G)
+	return h
+
+
+def escritura (fileName = 'collatz.txt'):
+	archivo = open(fileName, 'w')
+	archivo.write('Esper, calculando..')
+	for i in range(2,N):
+	        p = secuencia (i)
+        	archivo.write(str(p))
+
+
+escritura()
+
+
+
 
